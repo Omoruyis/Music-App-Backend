0994d48f80b7de2f25432d12397019e86f02d070
unknown <omoruyi.isaac97@gmail.com>
2020-02-25 15:57:14 +0100

    first-commit

---
+++ server/models/user.js
@@ -0,0 +1,121 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const _ = require('lodash')
+const bcrypt = require('bcryptjs')
+
+var UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        },
+    }, 
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    }, 
+    name: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }, 
+    'phone-number': {
+        type: Number,
+        required: true,
+        minlength: 1,
+        trim: true,
+    }, 
+    address: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }, 
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        }, 
+        token: {
+            type: String, 
+            required: true
+        }
+    }]
+})   
+
+/****MongooseSchema methods */
+UserSchema.methods.generateAuthToken = function () {
+    let user = this
+    const access = 'auth'
+    const token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString()
+
+    user.tokens = user.tokens.concat([{access, token}]) 
+
+    return user.save().then(() => {
+        return token
+    })  
+}
+
+
+
+/******MongooseSchema statics */
+UserSchema.statics.findByCredentials = function (email, password) {
+    const User = this;
+
+    return User.findOne({ email }).then(user => {
+
+        return new Promise((resolve, reject) => {
+            if (!user) {
+                resolve('Wrong email address')
+            }
+
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user)
+                }
+                else {
+                    resolve('Wrong password')
+                }
+            })
+        })
+    })
+}
+
+UserSchema.statics.findExistingEmail = function (email) {
+    const User = this
+
+    return User.findOne({ email }).then(user => {
+        return new Promise((resolve, reject) => {
+            if (user) {
+                resolve('User already exists')
+            } else {
+                resolve(null)
+            }
+        })
+    })
+}
+
+UserSchema.pre('save', function (next) {
+    const user = this;
+
+    if (!user.isModified('password')) next()
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            user.password = hash
+            next()
+        })
+    });
+})
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = { User };
---
+++ server/server.js
@@ -0,0 +1,79 @@
+require('./config/config')
+
+const _ = require('lodash')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const { mongoose } = require('./db/mongoose')
+const { User } = require('./models/user')
+
+const port =  process.env.PORT
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cors())
+
+/*** SignUp Route */
+app.post('/signup', (req, res) => {
+    let body = _.pick(req.body, ['email', 'password', 'firstname', 'lastname', 'phone-number'])
+
+    User.findExistingEmail(body.email).then(email => {
+        if (email) {
+            return res.send('User already exists')
+        }
+    })
+    
+    const user = new User(body)
+
+    user.save().then(() => {
+        return user.generateAuthToken()
+    }).then(() => {
+        res.send(user)
+    }).catch(e => {
+        console.log(e)
+        res.status(404).send(`New error found ${e}`)
+    })
+})
+
+/*****Login Route */
+app.post('/login', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password'])
+
+    User.findByCredentials(body.email, body.password).then(user => {
+        if (!user._id) {
+            return res.send(user)
+        } else {
+            return user.generateAuthToken().then(() => {
+                res.send(user)
+            })
+        }
+    }).catch(e => {
+        res.status(400).send(`not here and error: ${e}`)
+    })
+})
+
+/******Reset password */
+app.post('/reset', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password', 'newpassword'])
+
+    User.findByCredentials(body.email, body.password).then(user => {
+        if (!user._id) {
+            return res.send(user)
+        } else {
+            user.password = body.newpassword
+            user.save().then(user => {
+                res.send(user)
+            }).catch(e => res.status(400).send(e))
+        }
+    }).catch(e => {
+        res.status(400).send(`not here and error: ${e}`)
+    })
+})
+
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`)
+})
