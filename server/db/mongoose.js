37838932b0c35d002be0792d89d5006eec6d2a06
Omoruyis <omoruyi.isaac97@gmail.com>
2020-09-08 23:17:30 +0100

    update

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
+// const url = process.env.MONGODB_URI
 
 mongoose.Promise = global.Promise
-mongoose.connect(url, {useNewUrlParser: true})
+// mongoose.connect(url, {useNewUrlParser: true})
+mongoose.connect('mongodb+srv://Omoruyi:9wNtETe9km8o8Y5N@cluster0.f26bi.mongodb.net/Omoruyi?retryWrites=true&w=majority', {useNewUrlParser: true})
 mongoose.set('useCreateIndex', true);
+
+
 
 
 module.exports = {
